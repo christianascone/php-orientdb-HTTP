01ea57ba28ccbc6c84922fc15bba27a9df096d11
ChristianAscone <ascone.christian@gmail.com>
2017-09-20 22:51:08 +0200

    Add check for empty embeddable fields when using Record data for document post

--- src/Doctrine/ODM/OrientDB/Data/Record.php
+++ src/Doctrine/ODM/OrientDB/Data/Record.php
@@ -300,8 +300,33 @@ class Record {
         return json_encode($this->getoData());
     }
 
+    /**
+     * Get an associative array of Record data
+     * parsing empty EMBEDDABLE fields (namely array parameters)
+     * in null values
+     *
+     * @return array
+     */
+    public function getoDataForPost() {
+        // If oData is empty, returns oData
+        if(empty($this->oData))
+            return $this->getOData();
+        $oDataForPost = [];
+        foreach ($this->oData as $key => $val){
+            // If field is an array (EMEDDABLE) and it is
+            // empty, set it as null
+            if(is_array($val) && empty($val)){
+                $oDataForPost[$key] = null;
+            }else{
+                $oDataForPost[$key] = $val;
+            }
+        }
+
+        return $oDataForPost;
+    }
+
     public function getRecordToPost(){
-        return json_encode($this->getoData() + array('@rid'=>$this->getRid()->jsonSerialize()));
+        return json_encode($this->getoDataForPost() + array('@rid'=>$this->getRid()->jsonSerialize()));
     }
 
 }
